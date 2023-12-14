833c6094230f2b87e98bc9666807cba6c8b16b8a
Vjy05git <99934278+Vjy05git@users.noreply.github.com>
2023-12-14 12:31:50 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 docker-compose up -d
 
 #tagging the image:
-docker tag nodejs:web ravivarman46/nodejs:cicd
+docker tag nodejs:web vjyguvi/nodejs:cicd
 
 #pushing an image to dockerhub:
-docker push ravivarman46/nodejs:cicd
+docker push vjyguvi/nodejs:cicd
 echo "the image has been pushed to docker hub"
